cb732415ce71874ebb1a8c9ffe2cd5f0aaaeb6a7
Rajeshsharma-123 <079bct060@ioepc.edu.np>
2025-06-06 18:22:12 +0545

    added  caching features to the static routes

--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,48 +1,31 @@
-import React from 'react'
-import { Label } from '@radix-ui/react-label'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
-import { Button } from '@/components/ui/button'
-import { prisma } from '@/lib/prisma'
-import { redirect } from "next/navigation";
-import next from 'next'
-
+"use client"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { useActionState } from "react";
+import * as actions from "@/actions";
+ 
 const CreateSnippetPage = () => {
-     
-    async function createSnippet(formData:FormData) {
-        "use server"  // use server directive
-        const title = formData.get("title") as string;
-        const code = formData.get("code") as string;
 
-        const snippet = await prisma.snippet.create({
-            data: {
-                title,
-                code
-            }
-        });
-        console.log("created snippet", snippet);
-
-        redirect("/");
-    }
+  const [formStateData, xyz ] = useActionState(actions.createSnippet, {message:""});
 
 
-
+    
   return (
-    <form action={createSnippet}>
+    <form action={xyz}>
       <div>
         <Label>Title</Label>
-        <Input type='text' name='title' id='title'/>
-        
+        <Input type="text" name="title" id="title" />
       </div>
-
       <div>
         <Label>Code</Label>
-        <Textarea  name='code' id='code'/>
-        
-      </div>
-      <Button type="submit" className='my-4 bg-black text-white' >New</Button>
+        <Textarea name="code" id="code" />
+      </div> 
+      {formStateData.message && <div className="p-2 bg-red-300 border-2 border-red-600 mt-2">{formStateData.message}</div>}
+      <Button type="submit" className="my-4 bg-black text-white">New</Button>
     </form>
-  )
-}
+  );
+};
 
-export default CreateSnippetPage
+export default CreateSnippetPage;
